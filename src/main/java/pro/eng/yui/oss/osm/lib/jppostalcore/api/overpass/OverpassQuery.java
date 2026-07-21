2556da37d0d85d13b0aa17a32c24fe42a2fbe9ef
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-22 06:55:25 +0900

    add JavaDoc comment

--- src/main/java/pro/eng/yui/oss/osm/lib/jppostalcore/api/overpass/OverpassQuery.java
+++ src/main/java/pro/eng/yui/oss/osm/lib/jppostalcore/api/overpass/OverpassQuery.java
@@ -4,6 +4,7 @@ public class OverpassQuery {
     
     private OverpassQuery(){ /* util */ }
     
+    /** 都道府県単位で抽出 */
     public static String getPostSearchQuery(String prefName){
         return
             "area[\"boundary\"=\"administrative\"][\"admin_level\"=\"4\"][\"name\"=\""+ prefName +"\"]->.a;"+
