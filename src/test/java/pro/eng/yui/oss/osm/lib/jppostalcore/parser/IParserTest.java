55c60774d9c02e8b167f99796b2d0ba07fcc0e39
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-23 09:14:32 +0900

    テストケース更新

--- src/test/java/pro/eng/yui/oss/osm/lib/jppostalcore/parser/IParserTest.java
+++ src/test/java/pro/eng/yui/oss/osm/lib/jppostalcore/parser/IParserTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IParserTest {
     
-    class IParserMock implements IParser<Object>{
+    static class ParserMock extends AbstParser<Object>{
         @Override
         public Map<Days, Object> decode(TextValue tagValue) { return Map.of(); }
         @Override
@@ -21,7 +21,7 @@ class IParserTest {
     void reverseOne(){
         String[] input = {"A"};
         String[] expected = {"A"};
-        IParserMock mock = new IParserMock();
+        ParserMock mock = new ParserMock();
         String[] result = mock.reverse(input);
         assertEquals(expected[0], result[0]);
     }
@@ -30,7 +30,7 @@ class IParserTest {
     void reverseTwo(){
         String[] input = {"A","Z"};
         String[] expected = {"Z","A"};
-        IParserMock mock = new IParserMock();
+        ParserMock mock = new ParserMock();
         String[] result = mock.reverse(input);
         assertEquals(expected[0], result[0]);
         assertEquals(expected[1], result[1]);
@@ -40,7 +40,7 @@ class IParserTest {
     void reverseEmpty(){
         String[] input = {};
         String[] expected = {};
-        IParserMock mock = new IParserMock();
+        ParserMock mock = new ParserMock();
         String[] result = mock.reverse(input);
         assertEquals(expected.length, result.length);
     }
