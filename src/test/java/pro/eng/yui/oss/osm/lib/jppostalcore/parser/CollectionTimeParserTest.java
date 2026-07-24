0306dd32bb65d5dbe0fd9e7437315fdab6a50fd7
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-25 06:52:17 +0900

    add test that fails

--- src/test/java/pro/eng/yui/oss/osm/lib/jppostalcore/parser/CollectionTimeParserTest.java
+++ src/test/java/pro/eng/yui/oss/osm/lib/jppostalcore/parser/CollectionTimeParserTest.java
@@ -41,6 +41,37 @@ class CollectionTimeParserTest {
     }
 
     @Test
+    void decodeBasicWithSpace() {
+        CollectionTimes input = new CollectionTimes("Mo-Fr 10:00, 15:00; Sa-Su,PH 11:00");
+
+        CollectionTimeParser parser = new CollectionTimeParser();
+        Map<Days, CollectionTimeParser.DaySchedule> result = parser.decode(input);
+
+        assertEquals(8, result.size());
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.MONDAY).status());
+        assertEquals(new CollectionTime("10:00"), result.get(Days.MONDAY).schedule().get(0));
+        assertEquals(new CollectionTime("15:00"), result.get(Days.MONDAY).schedule().get(1));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.TUESDAY).status());
+        assertEquals(new CollectionTime("10:00"), result.get(Days.TUESDAY).schedule().get(0));
+        assertEquals(new CollectionTime("15:00"), result.get(Days.TUESDAY).schedule().get(1));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.WEDNESDAY).status());
+        assertEquals(new CollectionTime("10:00"), result.get(Days.WEDNESDAY).schedule().get(0));
+        assertEquals(new CollectionTime("15:00"), result.get(Days.WEDNESDAY).schedule().get(1));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.THURSDAY).status());
+        assertEquals(new CollectionTime("10:00"), result.get(Days.THURSDAY).schedule().get(0));
+        assertEquals(new CollectionTime("15:00"), result.get(Days.THURSDAY).schedule().get(1));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.FRIDAY).status());
+        assertEquals(new CollectionTime("10:00"), result.get(Days.FRIDAY).schedule().get(0));
+        assertEquals(new CollectionTime("15:00"), result.get(Days.FRIDAY).schedule().get(1));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.SATURDAY).status());
+        assertEquals(new CollectionTime("11:00"), result.get(Days.SATURDAY).schedule().get(0));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.SUNDAY).status());
+        assertEquals(new CollectionTime("11:00"), result.get(Days.SUNDAY).schedule().get(0));
+        assertEquals(CollectionTimeParser.DayStatus.OPEN_DAY, result.get(Days.PUBLIC_HOLIDAY).status());
+        assertEquals(new CollectionTime("11:00"), result.get(Days.PUBLIC_HOLIDAY).schedule().get(0));
+    }
+
+    @Test
     void encodeBasic(){
         CollectionTimes input = new CollectionTimes("Mo-Fr 10:00,17:00; Sa-Su,PH 9:00");
 
